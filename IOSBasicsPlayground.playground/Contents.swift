892ebee4b059c19930f1406a92818c7bcfbd7bb1
KutayMehmetOzkoc <kutayozkoc999@gmail.com>
2025-12-03 12:50:59 +0300

    swift basics

---
+++ IOSBasicsPlayground.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+// Variables & Constants
+//snake_case
+//camelCase (Swift Write Standart)
+
+//codes works on sequence
+
+var userName = "James"
+var surName = "Hetfield";
+
+userName = "Lars"
+print(userName)
+
+let userAge = 50
+//userAge = 30 can not change let variables.
+
+let score = userAge * 12 // we can use let variables for operations
+
+
+//String
+var x = "x"
+userName.append("o")
+userName.lowercased()
+userName.uppercased()
+
+
+//Numbers
+/*
+ integer & double & float
+ */
+
+var age = 12 // Integer
+var pi = 3.14 // float || double
+
+//Boolean true or false
+
+var myBoolean = false
+
+// Casting
+// Class names start with uppercase
+var myString : String = "myString" // We casted myString as String
+var myInteger : Int32 = 12
+var myDouble : Double = 12.5
+
+var intToDouble : Int = Int(12.5)
+var numberToString : String = String(123)
+
+let initString : String
+
+initString = "initString"
+
+initString.uppercased()
+let myUpperVar = initString.uppercased()
+let myLowerVar = initString.lowercased()
+
