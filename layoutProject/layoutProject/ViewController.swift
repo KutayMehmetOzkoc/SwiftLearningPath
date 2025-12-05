1d4b560b788ba4835a5fb11bc88842fa33c20326
KutayMehmetOzkoc <kutayozkoc999@gmail.com>
2025-12-05 22:24:25 +0300

    constraints app

---
+++ layoutProject/layoutProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  layoutProject
+//
+//  Created by Kutay Mehmet Ozko on 5.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
