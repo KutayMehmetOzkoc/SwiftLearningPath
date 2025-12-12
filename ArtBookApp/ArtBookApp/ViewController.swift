04d544712656364d3a9b39cf39e882aa004008f7
KutayMehmetOzkoc <kutayozkoc999@gmail.com>
2025-12-12 18:44:08 +0300

    add fetch data

--- ArtBookApp/ArtBookApp/ViewController.swift
+++ ArtBookApp/ArtBookApp/ViewController.swift
@@ -6,16 +6,66 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
 
     @IBOutlet weak var tableViewMain: UITableView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        tableViewMain.delegate = self
+        tableViewMain.dataSource = self
+        
         navigationController?.navigationBar.topItem?.title = "Art Book"
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addNewRow))
+        
+        getData()
+    }
+    
+    func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        
+        request.returnsObjectsAsFaults = false
+        do {
+            let results = try context.fetch(request)
+            for result in results as! [NSManagedObject]{
+                if let name =  result.value(forKey: "name") as? String{
+                    self.nameArray.append(name)
+                }
+                if let id = result.value(forKey: "id") as? UUID{
+                    self.idArray.append(id)
+                }
+            }
+            self.tableViewMain.reloadData()
+            print("Refresh Succes Data List")
+        }
+        catch{
+            print("Error fetching data")
+        }
+        
+        
     }
 
     @objc func addNewRow() {
