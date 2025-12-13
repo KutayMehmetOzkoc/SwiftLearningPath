06d6faef5b92cbdf408196f9af565d114872301f
KutayMehmetOzkoc <kutayozkoc999@gmail.com>
2025-12-13 14:38:57 +0300

    artBookProgress

--- ArtBookApp/ArtBookApp/ViewController.swift
+++ ArtBookApp/ArtBookApp/ViewController.swift
@@ -10,9 +10,10 @@ import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    
     var nameArray = [String]()
     var idArray = [UUID]()
+    var selectedPainting = ""
+    var selectedPaintingId : UUID?
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return nameArray.count
@@ -24,10 +25,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
-
     @IBOutlet weak var tableViewMain: UITableView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -41,7 +40,30 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         getData()
     }
     
-    func getData(){
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPainting = nameArray[indexPath.row]
+        selectedPaintingId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "toDetailsVC" {
+                let destinationVC = segue.destination as! detailViewController
+                destinationVC.chosenPaint = selectedPainting
+                destinationVC.chosenPaintingId = selectedPaintingId
+            }
+        }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newDataCome"), object: nil)
+    }
+    
+    @objc func getData(){
+        
+        //To remove dublicate data
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
@@ -50,25 +72,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         request.returnsObjectsAsFaults = false
         do {
             let results = try context.fetch(request)
-            for result in results as! [NSManagedObject]{
-                if let name =  result.value(forKey: "name") as? String{
-                    self.nameArray.append(name)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject]{
+                    if let name =  result.value(forKey: "name") as? String{
+                        self.nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID{
+                        self.idArray.append(id)
+                    }
                 }
-                if let id = result.value(forKey: "id") as? UUID{
-                    self.idArray.append(id)
-                }
+                self.tableViewMain.reloadData()
+                print("Refresh Succes Data List")
             }
-            self.tableViewMain.reloadData()
-            print("Refresh Succes Data List")
         }
         catch{
             print("Error fetching data")
         }
-        
-        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                self.nameArray.remove(at: indexPath.row)
+                self.idArray.remove(at: indexPath.row)
+                tableViewMain.deleteRows(at: [indexPath], with: .fade)
+            }
     }
 
     @objc func addNewRow() {
+        selectedPainting = ""
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
 }
--- ArtBookApp/ArtBookApp/detailViewController.swift
+++ ArtBookApp/ArtBookApp/detailViewController.swift
@@ -14,9 +14,53 @@ class detailViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBOutlet weak var textFieldName: UITextField!
     @IBOutlet weak var textFieldYear: UITextField!
     @IBOutlet weak var textFieldArtistName: UITextField!
+    
+    var chosenPaint = ""
+    var chosenPaintingId : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if(chosenPaint != ""){
+            //CoreData
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = chosenPaintingId?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let resultName = result.value(forKey: "name") as? String{
+                            textFieldName.text = resultName
+                        }
+                        if let resultArtist = result.value(forKey: "artist") as? String{
+                            textFieldArtistName.text = resultArtist
+                        }
+                        if let resultYear = result.value(forKey: "year") as? Int{
+                            textFieldYear.text = String(resultYear)
+                        }
+                        if let resultImage = result.value(forKey: "image") as? Data{
+                            let imageUI = UIImage(data: resultImage)
+                            imageViewDetail.image = imageUI
+                        }
+                    }
+                }
+            }
+            catch{
+                
+            }
+        }
+        else {
+            textFieldName.text = ""
+            textFieldYear.text = ""
+            textFieldArtistName.text = ""
+        }
         
         // Recognizers
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
@@ -29,8 +73,6 @@ class detailViewController: UIViewController, UIImagePickerControllerDelegate, U
 
     }
 
-    
-    
     @objc func imageTapped(){
         let picker = UIImagePickerController()
         picker.delegate = self
@@ -52,7 +94,6 @@ class detailViewController: UIViewController, UIImagePickerControllerDelegate, U
         print("Button Clicked")
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        
         let newArt = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
         newArt.setValue(textFieldName.text, forKey: "name")
         newArt.setValue(textFieldArtistName.text, forKey: "artist")
@@ -64,9 +105,18 @@ class detailViewController: UIViewController, UIImagePickerControllerDelegate, U
         newArt.setValue(data, forKey: "image")
         do{
             try context.save()
+            
             print("success")
+            
+            NotificationCenter.default.post(name: NSNotification.Name("newDataCome"), object: nil)
+            
+            self.navigationController?.popViewController(animated: true)
         }catch {
             print("Error saving context \(error)")
+            let alert = UIAlertController(title: "Error Has Occured", message: "Error While saving Context Try Again ‼️", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
         }
     }
 }
